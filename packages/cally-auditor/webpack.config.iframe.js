515a420eb165c9a0ab5943e7958acbbcce2cfb67
taejs <taeshindev@gmail.com>
2021-08-30 01:28:04 +0900

    feat(cally-auditor): create webpack config
    reference to react-overlay

---
+++ packages/cally-auditor/webpack.config.iframe.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  entry: "./src/iframe/index.js",
+  output: {
+    path: path.join(__dirname, "./lib"),
+    filename: "iframe-bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.js$/,
+            include: [path.resolve(__dirname, "./src")],
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-react"],
+              },
+            },
+          },
+          {
+            test: /\.js$/,
+            exclude: /@babel(?:\/|\\{1,2})runtime/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                babelrc: false,
+                configFile: false,
+                compact: false,
+                presets: ["@babel/preset-react"],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      // This prevents our bundled Iframe from accidentally hijacking devtools.
+      __CALLY_AUDITOR_GLOBAL_HOOK__: "({})",
+    }),
+  ],
+  performance: false,
+};
---
+++ packages/cally-auditor/webpack.config.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const path = require("path");
+
+module.exports = {
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "./lib"),
+    filename: "index.js",
+    library: "CallyAuditorOverlay",
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /iframe-bundle\.js$/,
+        use: "raw-loader",
+      },
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, "./src"),
+        use: "babel-loader",
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      iframeScript$: path.resolve(__dirname, "./lib/iframe-bundle.js"),
+    },
+  },
+  optimization: {
+    nodeEnv: false,
+  },
+  performance: {
+    hints: false,
+  },
+};
